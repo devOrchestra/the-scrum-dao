1c5af15e283ebadaa186cebccb52c99b779729fd
Роман Каганов <q2mber@icloud.com>
2017-10-25 14:19:06 +0300

    SD-1 #start first Project contract implementation

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,42 +1,22 @@
 pragma solidity ^0.4.11;
 
 
-contract Project {
-  address owner;
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-  struct UserStory {
-  bytes32 hash;
-  string track;
-  address requestor;
-  string story;
-  uint date;
-  uint votes;
-  uint avgEstimatedTime;
-  mapping (address => bool) votedPriority;
-  mapping (address => bool) votedEstimatedTime;
-  }
 
-  struct SprintVoiting {
-  bytes32[] sprintBacklogHashes;
-  uint date;
-  uint votes;
-  bool started;
-  bool finished;
-  mapping (address => bool) voted;
-  mapping (bytes32 => UserStory) sprintBacklog;
-  }
+contract Project is StandardToken, Ownable {
+  string public name;
+  string public symbol;
+  uint8 public decimals;
 
-  bytes32[] sprintVoitingsHashes;
-
-  mapping (bytes32 => SprintVoiting) public sprintVoitings;
-
-  UserStory[] public productBacklog;
+  uint storyPointMultiplier = 10;
 
   address[] public workers;
 
-  address public master;
+  mapping (string => address) workersLogins;
 
-  address[] public investors;
+  address trustedOracle;
 
   modifier onlyTeem {
     bool result = false;
@@ -46,38 +26,30 @@ contract Project {
         break;
       }
     }
-    if (msg.sender == master) {
-      result = true;
-    }
     require(result);
     _;
   }
 
-  modifier onlyMaster {
-    require(msg.sender == master);
+  modifier onlyTrustedOracle {
+    require(msg.sender == trustedOracle);
     _;
   }
 
-  function Project(){
-    owner = msg.sender;
+  function Project(string tokenName,string tokenSymbol, uint8 tokenDecimals, address oracle){
+    name=tokenName;
+    symbol=tokenSymbol;
+    decimals = tokenDecimals;
+    trustedOracle = oracle;
   }
 
-  function setMaster(address masterAddress){
-    require(msg.sender == owner);
-    master = masterAddress;
-  }
-
-  function addWorker(address worker){
-    require(msg.sender == owner);
+  function addWorker(address worker, string login) onlyOwner {
     bool alreadyAdded = false;
-    for (uint i = 0; i < workers.length; i++) {
-      if (worker == workers[i]) {
-        alreadyAdded = true;
-        break;
-      }
+    if (workersLogins[login] != 0) {
+      alreadyAdded = true;
     }
     require(!alreadyAdded);
     workers.push(worker);
+    workersLogins[login] = worker;
   }
 
   function getWorkersLength() public constant returns (uint){
@@ -88,97 +60,12 @@ contract Project {
     return workers[i];
   }
 
-  function addInvestor(address investor){
-    require(msg.sender == owner);
-    bool alreadyAdded = false;
-    for (uint i = 0; i < investors.length; i++) {
-      if (investor == investors[i]) {
-        alreadyAdded = true;
-        break;
-      }
-    }
-    require(!alreadyAdded);
-    investors.push(investor);
-  }
-
-  function getInvestorsLength() public constant returns (uint){
-    return investors.length;
-  }
-
-  function getInvestor(uint i) public constant returns (address){
-    return investors[i];
-  }
-
-  function addUserStory(string track, string story) returns (uint){
-    bytes32 hash = bytes32(sha3(now, story));
-    productBacklog.push(UserStory(hash, track, msg.sender, story, now, 0, 0));
-    return productBacklog.length;
-  }
-
-  function votePriority(uint i){
-    require(!productBacklog[i].votedPriority[msg.sender]);
-    productBacklog[i].votedPriority[msg.sender] = true;
-    productBacklog[i].votes += 1;
-  }
-
-  function isVoited(uint i) returns (bool){
-    return productBacklog[i].votedPriority[msg.sender];
-  }
-
-  function getUserStory(uint i) public returns (string, address, string, uint, uint, uint) {
-    return (productBacklog[i].track, productBacklog[i].requestor, productBacklog[i].story, productBacklog[i].date, productBacklog[i].votes, productBacklog[i].avgEstimatedTime);
-  }
-
-  function getBacklogLength() public constant returns (uint){
-    return productBacklog.length;
-  }
-
-  function voteEstimatedTime(uint i, uint time) onlyTeem {
-    require(!productBacklog[i].votedEstimatedTime[msg.sender]);
-    productBacklog[i].votedEstimatedTime[msg.sender] = true;
-    if (productBacklog[i].avgEstimatedTime > 0) {
-      productBacklog[i].avgEstimatedTime = (productBacklog[i].avgEstimatedTime + time) / 2;
-    }
-    else {
-      productBacklog[i].avgEstimatedTime = time;
+  function payAward(string login, uint256 points) public onlyTrustedOracle {
+    address recepient = workersLogins[login];
+    if (recepient != 0 && points > 0) {
+      uint256 awardSupply = points.mul(storyPointMultiplier);
+      balances[recepient] = balances[recepient].add(awardSupply);
+      totalSupply = totalSupply.add(awardSupply);
     }
   }
-
-  function initiateSprintVoiting(uint[] ids){
-    bytes32 hash = bytes32(sha3(now));
-    sprintVoitingsHashes.push(hash);
-    sprintVoitings[hash].date = now;
-    sprintVoitings[hash].votes = 0;
-    sprintVoitings[hash].started = true;
-    sprintVoitings[hash].finished = false;
-
-    for (uint i = 0; i < ids.length; i++) {
-      sprintVoitings[hash].sprintBacklogHashes.push(productBacklog[ids[i]].hash);
-    }
-  }
-
-  function getSprintVoitingsLength() returns (uint){
-    return sprintVoitingsHashes.length;
-  }
-
-  function getSprintVoitingHash(uint i) returns (bytes32){
-    return sprintVoitingsHashes[i];
-  }
-
-  function getLastSprintVoiting() returns (uint, uint, bool, bool, uint, bytes32){
-    SprintVoiting last = sprintVoitings[sprintVoitingsHashes[sprintVoitingsHashes.length-1]];
-    return (last.date, last.votes, last.started, last.finished, last.sprintBacklogHashes.length, sprintVoitingsHashes[sprintVoitingsHashes.length-1]);
-  }
-
-  function getSprintBacklogItem(bytes32 hash, uint i) returns (string, address, string, uint, uint, uint){
-    SprintVoiting sprint = sprintVoitings[hash];
-    bytes32 storyHash = sprint.sprintBacklogHashes[i];
-    UserStory story = sprint.sprintBacklog[storyHash];
-    return (story.track, story.requestor, story.story, story.date, story.votes, story.avgEstimatedTime);
-  }
-
-  function getStoryHash() returns {
-
-  }
-//  function get
 }
