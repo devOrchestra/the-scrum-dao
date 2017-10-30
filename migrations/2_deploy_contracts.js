680f7f9e796f1ab062dd521f4984acd8f7282f62
Роман Каганов <q2mber@icloud.com>
2017-10-30 19:47:40 +0300

    add Crowdsale to migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,15 @@
 var Project = artifacts.require("./Project.sol");
 var StoryPointsVoting = artifacts.require("./StoryPointsVoting.sol");
 var ProductBacklog = artifacts.require("./ProductBacklog.sol");
+var Crowdsale = artifacts.require("./Crowdsale.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Project,"SCRUM-DAO", "SAO", 18).then(function() {
     return deployer.deploy(StoryPointsVoting, Project.address);
   }).then(function() {
     return deployer.deploy(ProductBacklog, Project.address);
+  }).then(function() {
+    return deployer.deploy(Crowdsale, Project.address);
   });
 };
 
