be98c455fe2517aa74428e6592fc4bbb6d0443be
krabradosty <krabradosty@gmail.com>
2017-11-09 22:35:21 +0300

    ADDED config file for the server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,20 +1,23 @@
 import http = require('http');
+import loadConfig = require('ini-config');
+import path = require('path');
 
 import logger from './logger';
 import expressServer from './express';
 import JiraConnector from './jira';
 
+loadConfig(path.resolve(__dirname, 'config.ini'), (error, config) => {
+  if (error) throw error;
+  expressServer.set('jira', new JiraConnector());
+  expressServer.set('config', config);
 
-/**
- * INITIALIZING JIRA API CONNECTOR
- */
-expressServer.set('jira', new JiraConnector());
-
-/**
- * STARTING HTTP SERVER
- */
-let port: number = process.env.SERVER_PORT || 8021;
-let server: http.Server = http.createServer(expressServer);
-server.listen(port, function () {
-  logger.info("Express server listening on port " + port);
+  /**
+   * STARTING HTTP SERVER
+   */
+  let port: number = config.port;
+  let server: http.Server = http.createServer(expressServer);
+  server.listen(port, function () {
+    logger.info("Express server listening on port " + port);
+  });
 });
+
