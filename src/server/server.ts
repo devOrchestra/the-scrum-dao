8d7072600d7e7316298f3bfe6e5ce7a5fe6fa4b6
krabradosty <krabradosty@gmail.com>
2017-11-10 10:46:33 +0300

    ADDED tasks handler as background process

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,27 +1,46 @@
 import http = require('http');
 import loadConfig = require('ini-config');
 import path = require('path');
+import cluster = require('cluster');
 
 import logger from './logger';
 import expressServer from './express';
 import JiraConnector from './jira';
 import TaskScheduler from './taskScheduler';
+import TaskHandler from "./taskHandler";
 
-loadConfig(path.resolve(__dirname, 'config.ini'), (error, config) => {
-  if (error) throw error;
-  
-  let scheduler = new TaskScheduler(config);
-  
+if (cluster.isMaster) {
   /**
-   * STARTING HTTP SERVER
+   * STARTS MAIN SERVER
    */
-  expressServer.set('scheduler', scheduler);
-  expressServer.set('jira', new JiraConnector());
-  expressServer.set('config', config);
-  let port: number = config.port;
-  let server: http.Server = http.createServer(expressServer);
-  server.listen(port, function () {
-    logger.info("Express server listening on port " + port);
+  loadConfig(path.resolve(__dirname, 'config.ini'), (error, config) => {
+    if (error) throw error;
+
+    let scheduler = new TaskScheduler(config);
+    let tasksHandler = cluster.fork();
+    tasksHandler.on('exit', (code, signal) => {
+      logger.info(`Tasks handler died with code ${code}`);
+    });
+    
+    expressServer.set('scheduler', scheduler);
+    expressServer.set('jira', new JiraConnector());
+    expressServer.set('config', config);
+    let port: number = config.port;
+    let server: http.Server = http.createServer(expressServer);
+    server.listen(port, function () {
+      logger.info("Express server listening on port " + port);
+    });
   });
-});
+
+} else {
+  /**
+   * STARTS BACKGROUND TASKS HANDLER
+   */
+  loadConfig(path.resolve(__dirname, 'config.ini'), (error, config) => {
+    if (error) throw error;
+    
+    new TaskHandler(config);
+  });
+}
+
 
