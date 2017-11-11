ad996930f8e9cd9df664a2cfe6f125fdbce167b0
krabradosty <krabradosty@gmail.com>
2017-11-11 18:04:44 +0300

    ADDED loading admin private key from V3 file (server)

--- src/server/ethController.ts
+++ src/server/ethController.ts
@@ -1,11 +1,26 @@
-import web3 = require('web3');
+import Web3 = require('web3');
 import EthereumTx = require('ethereumjs-tx');
+import ethUtils = require('ethereumjs-util');
+import Wallet = require('ethereumjs-wallet');
+import fs = require('fs');
 
 
 class EthController {
-  
+
   constructor(config) {
-    
+    this.web3 = new Web3(new Web3.providers.HttpProvider(config.ethereum.url));
+    fs.readFile(config.ethereum.admin.walletPath, (error, data) => {
+      this.adminKey = Wallet.fromV3(data.toString(), config.ethereum.admin.walletPassword).getPrivateKey().toString('hex');
+    });
+  }
+
+  // FIELDS
+  private config: any;
+  private web3: Web3;
+  private adminKey: string;
+
+  // METHODS
+  public sendTransaction(): void {
   }
 }
 
