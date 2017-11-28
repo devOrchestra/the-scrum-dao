5ea55da51829ffab86d0df8ff36e4c6fbae310d4
krabradosty <krabradosty@gmail.com>
2017-11-28 12:38:37 +0300

    ADDED configurations for stage environment

--- truffle.js
+++ truffle.js
@@ -1,30 +1,40 @@
-var Wallet = require('ethereumjs-wallet');
-var ProviderEngine = require("web3-provider-engine");
-var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-var Web3 = require("web3");
-var FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
+let Wallet = require('ethereumjs-wallet');
+let ProviderEngine = require("web3-provider-engine");
+let WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
+let Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+let Web3 = require("web3");
+let FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
 
 
 let address;
 let providerUrl;
 let engine;
-if (process.env.NODE_ENV === 'production') {
+
+if (process.env.NODE_ENV === 'development') {
+  providerUrl = 'http://localhost:8545';
+  let web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
+  address = web3.eth.accounts[0];
+  engine = web3.currentProvider;
+
+} else if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'stage') {
+
+  let walletPassword;
+  if (process.env.NODE_ENV === 'production') {
+    walletPassword = process.env.OWNER_WALLET_PASSWORD;
+    providerUrl = process.env.ETH_NODE_URL;
+  } else {
+    walletPassword = 'semen';
+    providerUrl = 'https://rinkeby.infura.io/QTeUiM06pSmTwLqjbcip';
+  }
+
   let ownerWalletData = require('./credentials/ownerWallet.json');
-  let ownerWallet = Wallet.fromV3(ownerWalletData, process.env.OWNER_WALLET_PASSWORD);
+  let ownerWallet = Wallet.fromV3(ownerWalletData, walletPassword);
   address = "0x" + ownerWallet.getAddress().toString("hex");
-  providerUrl = process.env.ETH_NODE_URL;
   engine = new ProviderEngine();
   engine.addProvider(new FilterSubprovider());
   engine.addProvider(new WalletSubprovider(ownerWallet, {}));
   engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
   engine.start(); // Required by the provider engine.
-
-} else if (process.env.NODE_ENV === 'development') {
-  providerUrl = 'http://localhost:8545';
-  let web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
-  address = web3.eth.accounts[0];
-  engine = web3.currentProvider;
 }
 
 module.exports = {
@@ -36,6 +46,11 @@ module.exports = {
     },
     production: {
       network_id: 3,    // Official ropsten network id
+      provider: engine, // Use our custom provider
+      from: address     // Use the address we derived
+    },
+    stage: {
+      network_id: "*",    // Official ropsten network id
       provider: engine, // Use our custom provider
       from: address     // Use the address we derived
     },
