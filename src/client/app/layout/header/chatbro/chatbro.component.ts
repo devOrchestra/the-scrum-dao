4177609f6872b749b2e13e50bcd30499611bc669
commercialsuicide <commercialsuiside@gmail.com>
2017-11-30 23:39:52 +0300

    ADDED: Chatbro read file and error handling

--- src/client/app/layout/header/chatbro/chatbro.component.ts
+++ src/client/app/layout/header/chatbro/chatbro.component.ts
@@ -1,5 +1,4 @@
 import { Component, AfterViewInit, ElementRef } from '@angular/core';
-import * as chatbro_json from '../../../../../../credentials/chatbro-id.json';
 
 @Component({
   selector: 'app-chatbro',
@@ -12,15 +11,30 @@ export class ChatbroComponent implements AfterViewInit {
   ) { }
 
   ngAfterViewInit() {
-    const chatbroId = (<any>chatbro_json).encodedChatId;
-    const s = document.createElement("script");
-    s.type = "text/javascript";
-    s.innerHTML = `
-      /* Chatbro Widget Embed Code Start */
-      function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','//www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}
-      /* Chatbro Widget Embed Code End */
-      ChatbroLoader({encodedChatId: '${chatbroId}'});
-    `;
-    this._elementRef.nativeElement.appendChild(s);
+    fetch('assets/chatbro-id.json')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('There is no "chatbro-id.json" file in "credentials" folder. Chat will not be loaded');
+      })
+      .then(data => {
+        if (data.encodedChatId) {
+          const s = document.createElement("script");
+          s.type = "text/javascript";
+          s.innerHTML = `
+            /* Chatbro Widget Embed Code Start */
+            function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','//www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}
+            /* Chatbro Widget Embed Code End */
+            ChatbroLoader({encodedChatId: '${data.encodedChatId}'});
+          `;
+          this._elementRef.nativeElement.appendChild(s);
+        } else {
+          throw new Error('You have to specify chat id in "chatbro-id.json" file like "encodedChatId": "CHAT_ID". Chat will not be loaded');
+        }
+      })
+      .catch(err => {
+        console.warn(err);
+      });
   }
 }
