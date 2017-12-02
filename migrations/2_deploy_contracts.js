6f824a18afbe64e0950c62c1506d432f739de629
krabradosty <krabradosty@gmail.com>
2017-12-02 12:01:03 +0300

    IMPROVED contracts deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,16 @@
-var Project = artifacts.require("./Project.sol");
-var PlanningPoker = artifacts.require("./PlanningPoker.sol");
-var ProductBacklog = artifacts.require("./ProductBacklog.sol");
-var Crowdsale = artifacts.require("./Crowdsale.sol");
+let Project = artifacts.require("./Project.sol");
+let PlanningPoker = artifacts.require("./PlanningPoker.sol");
+let ProductBacklog = artifacts.require("./ProductBacklog.sol");
+let Crowdsale = artifacts.require("./Crowdsale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Project,"SCRUM-DAO", "SCR", 18).then(function() {
-    return deployer.deploy(PlanningPoker, Project.address);
-  }).then(function() {
-    return deployer.deploy(ProductBacklog, Project.address);
-  }).then(function() {
-    return deployer.deploy(Crowdsale, Project.address);
-  });
+  deployer
+    .deploy(Project, "SCRUM-DAO", "SCR", 18)
+    .then(() => {
+      return Promise.all([
+        deployer.deploy(PlanningPoker, Project.address),
+        deployer.deploy(ProductBacklog, Project.address),
+        deployer.deploy(Crowdsale, Project.address)
+      ]);
+    });
 };
-
